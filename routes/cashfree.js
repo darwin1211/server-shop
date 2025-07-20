83ffb265926a7faf2642cd56b5945a99cdbd9b77
darwin1211 <77988437+darwin1211@users.noreply.github.com>
2025-07-21 00:26:53 +0530

    Update cashfree.js

--- routes/cashfree.js
+++ routes/cashfree.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const axios = require("axios");
 
 // POST /api/cashfree-token
-router.post("/cashfree-token", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { amount, userId, email, phone, name } = req.body;
     const orderId = "ORDER_" + Date.now();
