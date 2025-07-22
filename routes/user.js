0b92b02e42f6e4782dc789c3a919a8189361b68f
darwin1211 <77988437+darwin1211@users.noreply.github.com>
2025-07-23 03:35:30 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const sendEmailFun = require("../utils/sendEmail"); // adjust path if needed
 
